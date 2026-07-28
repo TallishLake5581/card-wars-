412d35a9d586cfc62122a456310e9a89eb4740d0
TallishLake5581 <134895708+TallishLake5581@users.noreply.github.com>
2026-07-28 07:13:02 +0400

    server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
